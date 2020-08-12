29562ab52488a555db9a86f85f32aa9ff16900ab
Tasha <tashingsworth@gmail.com>
2020-08-12 10:47:30 -0400

    Starting over fresh. Created custom middleware for logger, set up server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require('express');
+const express = require('express')
+const userRouter = require("./users/userRouter")
 
 const server = express();
+const port = 4000
+
+server.use(express.json())
+
+server.use(userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +14,13 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const time = new Date().toISOString()
+  console.log(`${time} ${req.method} ${req.url}`)
+}
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
 
 module.exports = server;
